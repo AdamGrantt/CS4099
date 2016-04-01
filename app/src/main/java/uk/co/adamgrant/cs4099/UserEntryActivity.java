e9ca51c8e3346361e2f08328f76d246fe572ea01
Adam Grant <ag215@st-andrews.ac.uk>
2016-04-01 02:33:33 +0100

    Data entry form created. Does not submit any data, or update "hours slept" yet

--- app/src/main/java/uk/co/adamgrant/cs4099/UserEntryActivity.java
+++ app/src/main/java/uk/co/adamgrant/cs4099/UserEntryActivity.java
@@ -5,9 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class UserEntryActivity extends AppCompatActivity {
+
+    EditText sleptAt;
+    EditText sleptUntil;
+    TextView sleptFor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,56 @@ public class UserEntryActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // CHECK TO SEE IF ALREADY ENTERED DATA - IF SO....
+//        initForm();
+        TextView date = (TextView)findViewById(R.id.yesterday_date);
+        date.setTextSize(16);
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("EEEE, MMMM d, yyyy");
+        date.setText("Sleep data for: " + format.format(c.getTime()));
     }
+
+    public void initForm() {
+//        TextView date = (TextView)findViewById(R.id.yesterday_date);
+
+        initListeners();
+    }
+
+    public void initListeners() {
+        sleptFor = (TextView)findViewById(R.id.slept_for);
+//        sleptAt = (EditText)findViewById(R.id.edit_sleep_at);
+//        sleptUntil = (EditText) findViewById(R.id.edit_sleep_until);
+
+        sleptAt.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+//                sleptFor.setText("You slept forString.valueOf(i) + " / " + String.valueOf(charCounts));
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+
+        sleptUntil.addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+//                sleptFor.setText(String.valueOf(i) + " / " + String.valueOf(charCounts));
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+    }
+
+    public void onSubmit(View v){
+        Toast.makeText(this, "Data Submitted", Toast.LENGTH_LONG).show();
+    }
+    // IF ENTRY FOR TODAY - REMOVE FORM FROM PAGE
+    // ON SUBMIT BUTTON - CLEARS FORM FROM PAGE
 
 }
