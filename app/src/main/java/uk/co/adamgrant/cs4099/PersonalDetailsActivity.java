8d7aee8b1a7c2a0fbeca38492a9ec08f0bff3b01
Adam Grant <ag215@st-andrews.ac.uk>
2016-04-03 22:35:42 +0100

    Added basic form for Personal Details entry.

---
+++ app/src/main/java/uk/co/adamgrant/cs4099/PersonalDetailsActivity.java
@@ -0,0 +1,41 @@
+package uk.co.adamgrant.cs4099;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class PersonalDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    // If personal data exists:
+    // Change title to - Your personal data.
+    // Change title/name/email hints to ones stored.
+    // Lock EditTexts?
+    // No Submit button
+
+    // On EDIT
+    // Unlock EditTexts
+    // Change title to - Please enter your personal details:
+    // Submit Button
+}
